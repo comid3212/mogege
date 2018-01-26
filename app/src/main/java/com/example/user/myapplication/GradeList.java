4a59663df70c594d89acadfad4f22ba082d44ef4
dick <dick@dick.com>
2018-01-26 15:28:05 +0800

    ㄐ掰

--- app/src/main/java/com/example/user/myapplication/GradeList.java
+++ app/src/main/java/com/example/user/myapplication/GradeList.java
@@ -3,11 +3,11 @@ package com.example.user.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Gradeshistory extends AppCompatActivity {
+public class GradeList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gradeshistory);
+        setContentView(R.layout.activity_grade_list);
     }
 }
